85aab4b4b4536eaf1d3c6613add496956d52500b
Josh Tynjala <joshtynjala@bowlerhat.dev>
2021-11-01 14:13:50 -0700

    flight-booker: better TextInput validation

--- flight-booker/src/Main.hx
+++ flight-booker/src/Main.hx
@@ -20,9 +20,31 @@ import openfl.events.Event;
 
 class Main extends Application {
 	private static final DATE_VALIDATOR = ~/^\d{4}\.\d{2}\.\d{2}$/;
+	private static final DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 	private static function validateDateString(value:String):Bool {
-		return DATE_VALIDATOR.match(value);
+		if (!DATE_VALIDATOR.match(value)) {
+			return false;
+		}
+
+		var dateParts = value.split(".");
+		var year = Std.parseInt(dateParts[0]);
+		var month = Std.parseInt(dateParts[1]);
+		var date = Std.parseInt(dateParts[2]);
+
+		if (month < 1 || month > 12) {
+			return false;
+		}
+
+		var daysInMonth = DAYS_IN_MONTH[month - 1];
+		if (month == 2 && ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+			daysInMonth = 29;
+		}
+		if (date < 1 || date > daysInMonth) {
+			return false;
+		}
+
+		return true;
 	}
 
 	public function new() {
@@ -60,6 +82,11 @@ class Main extends Application {
 		var date = now.getDate();
 		var nowString = year + "." + StringTools.lpad(Std.string(month), "0", 2) + "." + StringTools.lpad(Std.string(date), "0", 2);
 
+		// while a real app would use DatePicker or PopUpDatePicker, this 7GUIs
+		// task specifically asks for TextInput components because the point is
+		// to demonstrate "constraints" such as disabling the Book Flight button
+		// and showing an error state when the text cannot be parsed
+
 		this.departInput = new TextInput();
 		this.departInput.text = nowString;
 		this.departInput.addEventListener(Event.CHANGE, departInput_changeHandler);
@@ -79,13 +106,44 @@ class Main extends Application {
 	}
 
 	private function refreshAll():Void {
-		this.returnInput.enabled = this.flightTypeList.selectedIndex == 1;
+		var isRoundTrip = this.flightTypeList.selectedIndex == 1;
 
 		var departInputValid = validateDateString(this.departInput.text);
 		var returnInputValid = true;
 		if (this.returnInput.enabled) {
 			returnInputValid = validateDateString(this.returnInput.text);
 		}
+
+		if (departInputValid) {
+			this.departInput.errorString = null;
+		} else {
+			this.departInput.errorString = "Departure date is invalid";
+		}
+		if (returnInputValid) {
+			this.returnInput.errorString = null;
+		} else {
+			this.returnInput.errorString = "Return date is invalid";
+		}
+		if (isRoundTrip && departInputValid && returnInputValid) {
+			var departParts = this.departInput.text.split(".");
+			var returnParts = this.returnInput.text.split(".");
+			for (i in 0...3) {
+				var departPart = Std.parseInt(departParts[i]);
+				var returnPart = Std.parseInt(returnParts[i]);
+				if (departPart == returnPart) {
+					continue;
+				}
+				if (departPart > returnPart) {
+					departInputValid = false;
+					if (!departInputValid) {
+						this.departInput.errorString = "Departure date must be before return date";
+					}
+				}
+				break;
+			}
+		}
+
+		this.returnInput.enabled = isRoundTrip;
 		this.bookButton.enabled = departInputValid && returnInputValid;
 	}
 
